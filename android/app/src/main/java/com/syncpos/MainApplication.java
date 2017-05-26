44226e6b2ce0aa61fb54f369bc65b1570f3a24d3
Yaswanth <yaswanth.m@scarletwireless.com>
2017-05-26 20:03:37 +0530

    completed xix navigation integration in android

--- android/app/src/main/java/com/syncpos/MainApplication.java
+++ android/app/src/main/java/com/syncpos/MainApplication.java
@@ -7,34 +7,29 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+public class MainApplication extends  NavigationApplication  {
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
-  };
+     public boolean isDebug() {
+         // Make sure you are using BuildConfig from your own application
+         return BuildConfig.DEBUG;
+     }
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+     protected List<ReactPackage> getPackages() {
+         // Add additional packages you require here
+         // No need to add RnnPackage and MainReactPackage
+         return Arrays.<ReactPackage>asList(
+             // eg. new VectorIconsPackage()
+         );
+     }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+     @Override
+     public List<ReactPackage> createAdditionalReactPackages() {
+         return getPackages();
+     }
 }
