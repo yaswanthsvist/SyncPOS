141271f84036481307c211ca8c02faf2e14224d0
Yaswanth <yaswanthsvist@gmail.com>
2017-06-06 23:58:11 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,26 +12,23 @@ import {
   Button,
   View
 } from 'react-native';
-import { applyMiddleware, createStore } from 'redux';
-//import logger from 'redux-logger'
-/*const store = createStore(
-  reducer,
-  applyMiddleware(logger)
-)
-*/ 
+import logger from 'redux-logger'
 import { Navigation } from 'react-native-navigation';
 import {splashScreen} from './splash';
 import {loginScreen} from './login';
 import inventoryScreen from './inventory';
 import posScreen from './pos';
 import {sideLeftMenuScreen} from './containers/sideLeftMenu';
+import  configureStore  from './configureStore';
 
-Navigation.registerComponent('splashScreen', () => splashScreen);
-Navigation.registerComponent('loginScreen', () => loginScreen);
-Navigation.registerComponent('posScreen', () => posScreen);
-Navigation.registerComponent('inventoryScreen', () => inventoryScreen);
-Navigation.registerComponent('sideLeftMenuScreen', () => sideLeftMenuScreen);
+import {Provider} from "react-redux";
+const store=configureStore();
 
+Navigation.registerComponent('splashScreen', () => splashScreen,store,Provider);
+Navigation.registerComponent('loginScreen', () => loginScreen,store,Provider);
+Navigation.registerComponent('posScreen', () => posScreen,store,Provider);
+Navigation.registerComponent('inventoryScreen', () => inventoryScreen,store,Provider);
+Navigation.registerComponent('sideLeftMenuScreen', () => sideLeftMenuScreen,store,Provider);
 Navigation.startSingleScreenApp({
   screen: {
     screen: 'splashScreen', // unique ID registered with Navigation.registerScreen
