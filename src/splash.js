0d34723b25b3c08efcabfc48b2b9f92b0fa07719
Yaswanth <yaswanthsvist@gmail.com>
2017-06-06 23:59:17 +0530

    Update splash.js

--- src/splash.js
+++ src/splash.js
@@ -35,7 +35,7 @@ export class splashScreen extends Component {
       case 'didAppear':
             setTimeout(
               this.gotoLogin.bind(this)
-              ,2000
+              ,1000
             );
         break;
       case 'willDisappear':
